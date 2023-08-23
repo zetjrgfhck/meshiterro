a96b2d882e7fae6f08c7881f9ec56defe9942cff
HIDEKAZU SHINOHARA <141484383+zetjrgfhck@users.noreply.github.com>
2023-08-23 09:05:18 +0000

    [Finish] 4-10

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,9 @@ ActiveRecord::Schema.define(version: 2023_08_23_050540) do
   end
 
   create_table "post_images", force: :cascade do |t|
+    t.string "shop_name"
+    t.text "caption"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
