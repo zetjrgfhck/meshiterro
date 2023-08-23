67cd4755603cfad1de9dbc3ea01122bab03d2a61
HIDEKAZU SHINOHARA <141484383+zetjrgfhck@users.noreply.github.com>
2023-08-23 10:57:57 +0000

    [Finish] 4-12

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -17,7 +17,16 @@ class PostImagesController < ApplicationController
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
   end
+  
+  def destroy
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to '/post_images'
+  end
+
+  
   
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "homes/about" => "homes#about", as: 'about'
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 end
