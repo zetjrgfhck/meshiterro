55ce9416baa9f440e47538d479b342c348406053
HIDEKAZU SHINOHARA <141484383+zetjrgfhck@users.noreply.github.com>
2023-08-24 11:34:01 +0000

    [Finish] 4-19

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "homes/about" => "homes#about", as: 'about'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resources :post_comments,only: [:create]
+    resource :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
 end
