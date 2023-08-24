ce9cf25fd718cbd0351f359da104b3d04d7f821a
HIDEKAZU SHINOHARA <141484383+zetjrgfhck@users.noreply.github.com>
2023-08-24 10:27:09 +0000

    [Finish] 4-18

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
   :recoverable, :rememberable, :validatable
 
   has_many :post_images, dependent: :destroy
+  has_many :post_comments,dependent: :destroy
+  has_one_attached :profile_image
 
-has_one_attached :profile_image
-  
+
  def get_profile_image(width, height)
   unless profile_image.attached?
     file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
--- db/migrate/20230823050540_create_post_images.rb
+++ db/migrate/20230823050540_create_post_images.rb
@@ -4,6 +4,10 @@ class CreatePostImages < ActiveRecord::Migration[6.1]
       t.string :shop_name
       t.text :caption
       t.integer :user_id
+      t.integer :id
+      t.text :comment
+      t.integer :user_id
+      t.integer :post_image_id
       t.timestamps
     end
   end
