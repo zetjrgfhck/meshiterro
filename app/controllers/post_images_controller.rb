488bdb4ef233ef3927cfffff3094de8570c9db0b
HIDEKAZU SHINOHARA <141484383+zetjrgfhck@users.noreply.github.com>
2023-08-27 13:17:03 +0000

    [Finish] 4-22

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,28 +7,30 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
   end
 
   def index
-    @post_images = PostImage.all
-    @post_image = PostImage.new
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
     @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
   end
-  
+
   def destroy
     post_image = PostImage.find(params[:id])
     post_image.destroy
     redirect_to '/post_images'
   end
 
-  
-  
+
+
   private
 
   def post_image_params
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,7 +4,8 @@ class PostImage < ApplicationRecord
  belongs_to :user
  has_many :post_comments, dependent: :destroy
  has_many :favorites, dependent: :destroy
- 
+ validates :shop_name, presence: true
+ validates :image, presence: true
   
   def get_image
     unless image.attached?
